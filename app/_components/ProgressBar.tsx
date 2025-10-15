7f51627a6a13268c35c8c1d1b84c1326b285d132
Abdelrhman143 <aa632481@gmail.com>
2025-10-15 21:26:30 +0300

    finish progressBar

--- app/_components/ProgressBar.tsx
+++ app/_components/ProgressBar.tsx
@@ -7,11 +7,18 @@ export default function ProgressBar() {
     <div className="relative">
       <Progress value={value} className="mt-15 mb-10" />
       <span
-        className={`p-1 rounded-full border-2 border-[#C8C8C8] text-center absolute left-[${
+        className={`p-1 rounded-full border-2 text-[#485293] border-[#C8C8C8] text-center absolute left-[${
           value - 3
         }%] -top-12`}
       >
         you
+      </span>
+      <span
+        className={`text-center absolute left-[${
+          value - 3
+        }%] top-3 text-[#485293]`}
+      >
+        {value}%
       </span>
     </div>
   );
